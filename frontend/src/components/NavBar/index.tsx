4ff54ade4ccd0ec4aeba6a1af74a232e5294824c
Ana Luísa Dias <analudias@live.com>
2022-01-11 22:41:32 -0300

    Routes

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,7 +1,7 @@
 import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
 import './styles.css';
 
-function NavBar () {
+function Navbar () {
 
     return (
         <header>
@@ -21,4 +21,4 @@ function NavBar () {
 
 }
 
-export default NavBar;
+export default Navbar;
